7d0eea7d29794bb743d70e13228ad1e92ae44d50
etigafy <ch.schuster.graen@gmail.com>
2025-03-11 18:01:18 +0100

    [PRJ] feat: Add bluetooth
    Adding bluetooth section to waybar, the battery percentage of the used
    headphones are shown if available.

--- waybar/scripts/blue.sh
+++ waybar/scripts/blue.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-device_count=$(bluetoothctl devices | grep "Device" | wc -l)
-echo $device_count
+
+percentage="0"
+power=""
+bluetoothctl info > /dev/null 2>&1
+ret=$?
+if [ $ret -eq 0 ]; then
+	percentage=$(bluetoothctl info | grep Battery| sed 's/.*(\([0-9]*\)).*/\1/')
+	power=$(bluetoothctl show | grep PowerState | cut -d ':' -f2 | xargs)
+	if [ "$power" == "off" ]; then
+		percentage="0"
+	fi
+fi
+
+#device_count=$(bluetoothctl info | grep "Battery" | wc -l)
+echo "{\"percentage\": $percentage, \"class\": \"custom-bluetooth\"}"
